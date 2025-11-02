15f31a24c0af900f9aa2c8a17e20b41e89f1f333
Ramon Alberto Lima Cruz <98242989+ramoncruz192401@users.noreply.github.com>
2025-11-02 16:44:46 -0300

    Delete infra directory

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "tftfstatecurso18357"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
